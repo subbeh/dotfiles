84c364ad27b008c48f35222149bd6aac66b0a79f
Steven Terwindt <git@sbbh.cloud>
2026-08-03 11:38:55 +1000

    fix

--- statemate/.config/zsh/.zshrc.d/50-statemate.zsh
+++ statemate/.config/zsh/.zshrc.d/50-statemate.zsh
@@ -8,9 +8,11 @@ elif [ "$PROFILE_OS" = "linux" ]; then
   _bin=${XDG_PROJECTS_HOME}/statemate/dist/mate-linux-amd64
 fi
 
-source <($_bin completion zsh)
-
 if [ -x "$_bin" ]; then
   rm -f "${XDG_BIN_HOME}/mate"
   ln -s "$_bin" "${XDG_BIN_HOME}/mate" 2>/dev/null
+else
+  _bin=$(which mate)
 fi
+
+source <($_bin completion zsh)
