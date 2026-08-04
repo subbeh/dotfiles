937ff2dadc6899ef567e9d8de98dff593f62b091
Steven Terwindt <git@sbbh.cloud>
2026-08-04 10:57:54 +1000

    various

--- fd/.config/profile.d/50-fd.sh
+++ fd/.config/profile.d/50-fd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-alias f="fd --list-details --ignore-case --hidden --follow --hyperlink=auto --one-file-system --exclude .git"
-alias fdir="fd --type dir --list-details --ignore-case --hidden --follow --hyperlink=auto --one-file-system --exclude .git"
+alias f="fd --list-details --ignore-case --hidden --follow --hyperlink=auto --one-file-system"
+alias fdir="fd --type dir --list-details --ignore-case --hidden --follow --hyperlink=auto --one-file-system"
