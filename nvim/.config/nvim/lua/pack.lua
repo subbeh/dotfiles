b4effb3abdffa303ed34f2aefae93374f848bb42
Steven Terwindt <git@sbbh.cloud>
2026-08-17 14:11:14 +1000

    nvim

--- nvim/.config/nvim/lua/pack.lua
+++ nvim/.config/nvim/lua/pack.lua
@@ -14,6 +14,32 @@ local hooks = {
   },
 }
 
+vim.pack.clean = function(names, opts)
+  opts = opts or {}
+  local inactive = {}
+  names = vim
+    .iter(vim.pack.get(names, { info = false }))
+    :map(function(plug)
+      if not plug.active then
+        table.insert(inactive, plug.spec.name)
+      end
+      return plug.spec.name
+    end)
+    :totable()
+
+  if opts.inactive and not vim.tbl_isempty(inactive) then
+    vim.pack.del(inactive)
+  end
+
+  local plug_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site", "pack", "core", "opt")
+  for name, type in vim.fs.dir(plug_dir) do
+    if type == "directory" and not vim.tbl_contains(names, name) then
+      vim.fs.rm(vim.fs.joinpath(plug_dir, name), { recursive = true, force = true })
+      vim.notify(("vim.pack: Cleaned plugin '%s'"):format(name), vim.log.levels.INFO)
+    end
+  end
+end
+
 vim.api.nvim_create_autocmd("PackChanged", {
   group = vim.api.nvim_create_augroup("build_system", { clear = true }),
   callback = function(args)
