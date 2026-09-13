949c670753712477d5b777705d3646ea89a4456b
Steven Terwindt <git@sbbh.cloud>
2026-09-14 09:21:49 +1000

    feat(arch): add paclist and pacfind aliases

--- arch/.config/profile.d/30-pacman.sh
+++ arch/.config/profile.d/30-pacman.sh
@@ -2,4 +2,6 @@
 
 alias paczfl='pacman -Qq | fzf --preview "pacman -Qil {}" --layout=reverse --bind "enter:execute(pacman -Qil {} | less)"'
 alias paczfr='pacman -Slq | fzf --preview "pacman -Si {}" --layout=reverse'
+alias paclist='tv pacman-packages'
+alias pacfind='paru -Ss'
 alias yay='paru'
